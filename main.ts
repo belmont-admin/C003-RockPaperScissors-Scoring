b6fc4bf204b101babe445c29a51038c754e8c924
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2019-12-21 13:39:19 +0000

    Added a scoring system to use while playing an opponent

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let hand = 0
 input.onGesture(Gesture.Shake, function () {
     hand = Math.randomRange(1, 3)
     if (hand == 1) {
@@ -27,3 +26,26 @@ input.onGesture(Gesture.Shake, function () {
             `)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    score = 0
+    basic.showNumber(score)
+})
+input.onButtonPressed(Button.B, function () {
+    if (score == 9) {
+        score = 10
+        basic.showString("W")
+    } else {
+        score += 1
+        basic.showNumber(score)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (score > 0) {
+        score += -1
+        basic.showNumber(score)
+    }
+})
+let hand = 0
+let score = 0
+score = 0
+basic.showNumber(score)
