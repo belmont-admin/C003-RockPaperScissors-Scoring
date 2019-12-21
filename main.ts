79e5a0e35fb76a4f5bc7fb5fc6b601a15b0646d8
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2019-12-21 13:18:46 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-basic.forever(function () {
-	
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = Math.randomRange(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
 })
